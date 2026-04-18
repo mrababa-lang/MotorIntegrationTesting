22f3937c7ebdc5b29882720652d40ed7ec43fa73
mrababa-lang <mrababa@first.tech>
2026-04-18 09:10:09 +0400

    Fix ScenarioContext missing setter methods

--- insurance-quote-automation/src/test/java/com/insurance/automation/context/ScenarioContext.java
+++ insurance-quote-automation/src/test/java/com/insurance/automation/context/ScenarioContext.java
@@ -7,12 +7,10 @@ import io.restassured.response.Response;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import lombok.Data;
 
 /**
  * Scenario-scoped context shared across step definitions.
  */
-@Data
 public class ScenarioContext {
 
     private QuoteRequest currentRequest;
@@ -32,7 +30,123 @@ public class ScenarioContext {
     private String quoteRequestId;
     private List<String> offerIds = new ArrayList<>();
 
+    public QuoteRequest getCurrentRequest() {
+        return currentRequest;
+    }
+
+    public void setCurrentRequest(final QuoteRequest currentRequest) {
+        this.currentRequest = currentRequest;
+    }
+
+    public QuoteResponse getLastQuoteResponse() {
+        return lastQuoteResponse;
+    }
+
+    public void setLastQuoteResponse(final QuoteResponse lastQuoteResponse) {
+        this.lastQuoteResponse = lastQuoteResponse;
+    }
+
+    public ConfigStatusResponse getLastConfigResponse() {
+        return lastConfigResponse;
+    }
+
+    public void setLastConfigResponse(final ConfigStatusResponse lastConfigResponse) {
+        this.lastConfigResponse = lastConfigResponse;
+    }
+
+    public long getLastResponseTimeMs() {
+        return lastResponseTimeMs;
+    }
+
+    public void setLastResponseTimeMs(final long lastResponseTimeMs) {
+        this.lastResponseTimeMs = lastResponseTimeMs;
+    }
+
+    public int getLastHttpStatus() {
+        return lastHttpStatus;
+    }
+
+    public void setLastHttpStatus(final int lastHttpStatus) {
+        this.lastHttpStatus = lastHttpStatus;
+    }
+
+    public String getCurrentScenarioName() {
+        return currentScenarioName;
+    }
+
+    public void setCurrentScenarioName(final String currentScenarioName) {
+        this.currentScenarioName = currentScenarioName;
+    }
+
+    public String getCurrentScenarioId() {
+        return currentScenarioId;
+    }
+
+    public void setCurrentScenarioId(final String currentScenarioId) {
+        this.currentScenarioId = currentScenarioId;
+    }
+
+    public String getLastExpectedStatement() {
+        return lastExpectedStatement;
+    }
+
+    public void setLastExpectedStatement(final String lastExpectedStatement) {
+        this.lastExpectedStatement = lastExpectedStatement;
+    }
+
+    public String getRawLastResponse() {
+        return rawLastResponse;
+    }
+
+    public void setRawLastResponse(final String rawLastResponse) {
+        this.rawLastResponse = rawLastResponse;
+    }
+
+    public Response getLastApiResponse() {
+        return lastApiResponse;
+    }
+
+    public void setLastApiResponse(final Response lastApiResponse) {
+        this.lastApiResponse = lastApiResponse;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(final String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(final String customerId) {
+        this.customerId = customerId;
+    }
+
     public Optional<String> getCustomerLicenseId() {
         return Optional.ofNullable(customerLicenseId);
+    }
+
+    public void setCustomerLicenseId(final String customerLicenseId) {
+        this.customerLicenseId = customerLicenseId;
+    }
+
+    public String getQuoteRequestId() {
+        return quoteRequestId;
+    }
+
+    public void setQuoteRequestId(final String quoteRequestId) {
+        this.quoteRequestId = quoteRequestId;
+    }
+
+    public List<String> getOfferIds() {
+        return offerIds;
+    }
+
+    public void setOfferIds(final List<String> offerIds) {
+        this.offerIds = offerIds;
     }
 }
